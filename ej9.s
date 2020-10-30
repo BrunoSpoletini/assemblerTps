39d3d107412fd7393ff9b075ad1828c6b3cfdac3
BrunoSpoletini <bruno_spoletini@hotmail.com>
2020-10-29 22:08:23 -0300

    Array de funciones

---
+++ ej9.s
@@ -0,0 +1,51 @@
+#9) El programa que sigue, funcs, implementa void (*funcs[])() = {f1, f2, f3}. 
+#Compl´etelo para que la l´ınea con el comentario corresponda a funcs[entero](). 
+#Use el c´odigo m´as
+#eficiente.
+
+.data
+fmt: .string "%d"
+entero: .long -100
+funcs: 
+.quad f1
+.quad f2
+.quad f3
+.text
+f1: movl $0, %esi;
+movq $fmt, %rdi; 
+call printf; 
+jmp fin
+
+f2: 
+movl $1, %esi; 
+movq $fmt, %rdi; 
+call printf; 
+jmp fin
+
+f3: movl $2, %esi;
+movq $fmt, %rdi; 
+call printf; 
+jmp fin
+
+
+.global main
+main:
+pushq %rbp
+movq %rsp, %rbp
+# Leemos el entero.
+movq $entero, %rsi
+movq $fmt, %rdi
+xorq %rax, %rax
+call scanf
+xorq %rax, %rax
+
+movl entero, %ebx
+movq funcs(, %rbx, 8), %rdx
+
+
+jmp *%rdx
+
+fin:
+movq %rbp, %rsp
+popq %rbp
+ret
